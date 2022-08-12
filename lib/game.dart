bb96afa2f7ea72c6a5a9d43b8c904b1bdceb100d
Sulawut Sarkeat <sarkeat_s@silpakorn.edu>
2022-08-12 19:53:48 +0700

    Initial commit

---
+++ lib/game.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:math';
+
+class Game{
+  int answer = 0;
+  int count = 0;
+  Game(){
+    var r = Random();
+    answer = r.nextInt(100) + 1;
+    print('คำตอบคือ $answer');
+  }
+  int doGuess(int num){
+    if(num > answer){
+      print('➜ $num is too high! ▲');
+      count++;
+      return 0;
+    }
+    else if(num < answer){
+      print('➜ $num is too low! ▼');
+      count++;
+      return 0;
+    }
+    else{
+      count++;
+      print('➜ $num is correct ❤, total guesses: $count');
+      return 1;
+    }
+  }
+}
